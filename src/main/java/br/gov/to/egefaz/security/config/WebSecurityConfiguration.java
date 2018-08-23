124a4a6068dccc509d69553251d28f9bc34d8772
83633162100 <83633162100@c422917.sefaz.to.gov.br>
2018-08-23 12:39:22 -0300

    Merge origin/dev-arthur
    Conflicts:
    	pom.xml
    	src/main/webapp/WEB-INF/web.xml
    	src/main/webapp/pages/protected/index.xhtml
    	src/main/webapp/pages/public/login.xhtml
    	src/main/webapp/pages/template/footer.xhtml
    	src/main/webapp/pages/template/header.xhtml
    	src/main/webapp/pages/template/templateLogin.xhtml

--- src/main/java/br/gov/to/egefaz/security/config/WebSecurityConfiguration.java
+++ src/main/java/br/gov/to/egefaz/security/config/WebSecurityConfiguration.java
@@ -1,8 +1,6 @@
 package br.gov.to.egefaz.security.config;
 
-
 import br.gov.to.egefaz.security.service.UsuarioService;
-
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -21,75 +19,76 @@ import org.springframework.util.Assert;
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-  /**
-   * Configura o gerenciador de autenticacoes.
-   *
-   * @param auth gerenciado de autenticacao.
-   * @throws Exception falha ao configurar.
-   */
-  @Autowired
-  public final void configureGlobal(final AuthenticationManagerBuilder auth) throws Exception {
+    /**
+     * Configura o gerenciador de autenticacoes.
+     *
+     * @param auth gerenciado de autenticacao.
+     * @throws Exception falha ao configurar.
+     */
+    @Autowired
+    public final void configureGlobal(final AuthenticationManagerBuilder auth) throws Exception {
 
-    final UserDetailsService service = new UsuarioService();
-    final PasswordEncoder encoder = passwordEncoder();
+        final UserDetailsService service = new UsuarioService();
+        final PasswordEncoder encoder = passwordEncoder();
 
-    auth.userDetailsService(service).passwordEncoder(encoder);
-  }
+        auth.userDetailsService(service).passwordEncoder(encoder);
+    }
 
-  /**
-   * configura o filtro.
-   */
-  @Override
-  protected void configure(final HttpSecurity http) throws Exception {
+    /**
+     * configura o filtro.
+     */
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
 
-    final AuthenticationSuccessHandler sucessHandler = new CustomSucessAuthenticationHandler();
+        final AuthenticationSuccessHandler sucessHandler = new CustomSucessAuthenticationHandler();
 
-    http.authorizeRequests().antMatchers("/resources/**").permitAll().anyRequest().authenticated()
-        .and().formLogin().loginPage("/pages/public/login.xhtml").successHandler(sucessHandler)
-        .permitAll().and().logout().logoutSuccessUrl("/pages/public/login.xhtml").permitAll().and()
-        .csrf().disable();
-  }
+        http.authorizeRequests()
+                .antMatchers("/register", "/javax.faces.resource/**").permitAll().anyRequest().authenticated()
+                .and().formLogin().loginPage("/pages/public/login.xhtml").successHandler(sucessHandler)
+                .permitAll().and().logout().logoutSuccessUrl("/pages/public/login.xhtml").permitAll().and()
+                .csrf().disable();
+   
+    }
 
-  /**
-   * Encoder para senha no banco. Atualmente MD5.
-   *
-   * @return retorna o encoder.
-   */
-  public PasswordEncoder passwordEncoder() {
+    /**
+     * Encoder para senha no banco. Atualmente MD5.
+     *
+     * @return retorna o encoder.
+     */
+    public PasswordEncoder passwordEncoder() {
 
-    return new PasswordEncoder() {
+        return new PasswordEncoder() {
 
-      /**
-       * verifica se a senha informada e igual a mesma cifrada.
-       *
-       * @param rawPassword senha sem estar cifrada.
-       * @param encodedPassword senha cifrada.
-       * @return retorna true caso sejam iguais.
-       */
-      @Override
-      public boolean matches(final CharSequence rawPassword, final String encodedPassword) {
+            /**
+             * verifica se a senha informada e igual a mesma cifrada.
+             *
+             * @param rawPassword senha sem estar cifrada.
+             * @param encodedPassword senha cifrada.
+             * @return retorna true caso sejam iguais.
+             */
+            @Override
+            public boolean matches(final CharSequence rawPassword, final String encodedPassword) {
 
-        Assert.notNull(rawPassword);
-        Assert.notNull(encodedPassword);
+                Assert.notNull(rawPassword);
+                Assert.notNull(encodedPassword);
 
-        return encodedPassword.equals(encode(rawPassword));
-      }
+                return encodedPassword.equals(encode(rawPassword));
+            }
 
-      /**
-       * Cifra uma sequencia de caracteres.
-       *
-       * @param rawPassword sequencia de caracteres que sera cifrada.
-       * @return retorna a cadeia de caracteres cifrada.
-       */
-      @Override
-      public String encode(final CharSequence rawPassword) {
+            /**
+             * Cifra uma sequencia de caracteres.
+             *
+             * @param rawPassword sequencia de caracteres que sera cifrada.
+             * @return retorna a cadeia de caracteres cifrada.
+             */
+            @Override
+            public String encode(final CharSequence rawPassword) {
 
-        Assert.notNull(rawPassword);
+                Assert.notNull(rawPassword);
 
 //        return CriptoUtils.cryptWithMD5(String.valueOf(rawPassword));
-        return String.valueOf(rawPassword);
-      }
-    };
-  }
+                return String.valueOf(rawPassword);
+            }
+        };
+    }
 }
-
