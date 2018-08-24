c3d56caf61c69ccb96ea87f9c270fe4445ea57da
83633162100 <83633162100@c422917.sefaz.to.gov.br>
2018-08-24 10:11:56 -0300

    Merge origin/dev-arthur
    Conflicts:
    	pom.xml
    	src/main/webapp/WEB-INF/web.xml
    	src/main/webapp/pages/protected/index.xhtml
    	src/main/webapp/pages/public/login.xhtml
    	src/main/webapp/pages/template/footer.xhtml
    	src/main/webapp/pages/template/header.xhtml
    	src/main/webapp/pages/template/templateLogin.xhtml

---
+++ src/main/java/br/gov/to/egefaz/model/AbstractModel.java
@@ -0,0 +1,21 @@
+package br.gov.to.egefaz.model;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.hibernate.Session;
+
+public abstract class AbstractModel {
+	
+	@PersistenceContext
+	protected EntityManager em;
+	
+	public EntityManager getEm(){
+		return em;
+	}
+	
+	public Session getSession(){
+		 
+		 return em.unwrap(Session.class);
+	 }
+}
