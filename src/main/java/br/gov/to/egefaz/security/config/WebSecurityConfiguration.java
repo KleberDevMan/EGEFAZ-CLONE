0aa4d63ba0cb866ed8b8fb5b666c8bc3726ad417
83633162100 <83633162100@c422917.sefaz.to.gov.br>
2018-09-03 10:08:48 -0300

    Merge origin/dev-arthur
    Conflicts:
    	pom.xml
    	src/main/webapp/WEB-INF/web.xml
    	src/main/webapp/pages/protected/index.xhtml
    	src/main/webapp/pages/public/login.xhtml
    	src/main/webapp/pages/template/footer.xhtml
    	src/main/webapp/pages/template/header.xhtml
    	src/main/webapp/pages/template/templateLogin.xhtml

--- src/main/java/br/gov/to/egefaz/security/config/WebSecurityConfiguration.java
+++ src/main/java/br/gov/to/egefaz/security/config/WebSecurityConfiguration.java
@@ -1,13 +1,11 @@
 package br.gov.to.egefaz.security.config;
 
-import br.gov.to.egefaz.security.service.UsuarioService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.util.Assert;
