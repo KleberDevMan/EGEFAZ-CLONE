3436f51d0a3bb5a186eda4c11989f1817d3512cc
KleberDevMen <klebersubcontas@gmail.com>
2018-09-24 08:34:41 -0300

    .

--- src/main/java/br/gov/to/egefaz/security/view/DadosComplementaresView.java
+++ src/main/java/br/gov/to/egefaz/security/view/DadosComplementaresView.java
@@ -1,5 +1,6 @@
 package br.gov.to.egefaz.security.view;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,6 +8,7 @@ import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.bean.ViewScoped;
+import javax.servlet.http.Part;
 
 import br.gov.to.egefaz.security.domain.Escolaridade;
 import br.gov.to.egefaz.security.domain.Sexo;
@@ -24,8 +26,18 @@ public class DadosComplementaresView extends AbstractView{
 
     private static final String REDIRECT_PRIMEIRO_ACESSO = "primeiroAcesso?faces-redirect=true";
 	private UsuarioEgefaz usuario;
+	
+	private Part foto;
+	
+    public Part getFoto() {
+		return foto;
+	}
 
-    @PostConstruct
+	public void setFoto(Part foto) {
+		this.foto = foto;
+	}
+
+	@PostConstruct
     public void init() {
         if (usuario == null) {
         	this.usuario = (UsuarioEgefaz) pegaDaSessao(VariaveisSessao.USUARIO);
@@ -37,6 +49,15 @@ public class DadosComplementaresView extends AbstractView{
     }
     
     public String proceguir() {
+    	
+    	//salva a imagem no servidor
+    	try {
+			foto.write("/EGEFAZ/imagens" + foto.getSubmittedFileName());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    	
+    	
     	System.out.println("cadastrando dados complementares....");
         System.out.println(usuario);
         return "";
